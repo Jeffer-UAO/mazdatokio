6b93b57c2b419b704a90c9351d099201aefb4a08
Jeffer-UAO <jeffer443@gmail.com>
2023-05-23 12:50:43 -0500

    Primer commit

---
+++ src/components/FooterApp/FooterApp.js
@@ -0,0 +1,39 @@
+import { WhatsApp } from "../WhatsApp";
+import styles from "./FooterApp.module.scss";
+import { AiOutlineHome } from "react-icons/ai";
+import { MdOutlineCategory } from "react-icons/md";
+import { MdOutlineLocalOffer } from "react-icons/md";
+import { CiUser } from "react-icons/ci";
+import { BtnLink } from "../Common";
+
+export function FooterApp() {
+  return (
+    <div className={styles.btnWhatsapp}>
+      <div className={styles.paneluser}>
+        <BtnLink link={"/"} title={"HOME"} logo={<AiOutlineHome size={30} color="black" />} />
+        <BtnLink
+          link={"/featured"}
+          title={"EXCL"}
+          logo={<MdOutlineCategory size={30} color="black" />}
+        />
+
+        <WhatsApp
+          phoneNumber="+573103900666"
+          message="Hola, me gustaría obtener más información sobre sus productos."
+        />
+        <BtnLink
+          link={"/ofert"}
+          title={"OFER"}
+          logo={<MdOutlineLocalOffer size={30} color="black" />}
+        />
+        <BtnLink
+          link={
+            "https://majos.catalogointeractivo.com.co/admin-dashboard/"
+          }
+          title={"ADMI"}
+          logo={<CiUser size={30} color="black" />}
+        />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Layout/TopBar/TopBar.js
@@ -0,0 +1,25 @@
+import styles from "./TopBar.module.scss";
+import { CardImg } from "reactstrap";
+import Link from "next/link";
+
+export function TopBar() {
+  return (
+    <>
+      <div className={styles.topBar}>
+        <Link href="/">
+          <CardImg src="/image/logo.jpg" alt="5" />{" "}
+        </Link>
+
+        <div className={styles.title}>
+         
+        
+        </div>
+        <div className={styles.title}>         
+          <p className={styles.name}>MAZDA TOKIO AUTOPARTES</p>
+          <p className={styles.extra}>REPUESTOS ORIGINALES, DESPACHOS A TODA COLOMBIA</p>
+        </div>
+        
+      </div>
+    </>
+  );
+}
